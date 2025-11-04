ee9c8eb03f958e080e172e704d647f16eb649f93
theuzin999 <henriquefelipe946@gmail.com>
2025-11-04 01:13:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -203,14 +203,13 @@ def process_login(driver):
     return True
 
 def start_driver():
-    options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-software-rasterizer")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--remote-debugging-port=9222")
 
     return webdriver.Chrome(options=options)
 
